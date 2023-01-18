2206eeb31b4e350fcaa5e30a8b8535c7742c7aff
Birble.win <113261286+Birblewin@users.noreply.github.com>
2023-01-18 00:35:54 -0500

    first commit

---
+++ next-app/static/example_data.js
@@ -0,0 +1,65 @@
+export const data = {
+  stories: [
+    {
+      username: 'Chip Z',
+      image:
+        'https://pbs.twimg.com/profile_images/1492902736592969732/asuDqTgR_400x400.jpg',
+    },
+    {
+      username: 'cleverdude',
+      image:
+        'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
+    },
+    {
+      username: 'zucky',
+      image:
+        'https://i.insider.com/6329911dedbe7f00190ef740?width=1136&format=jpeg',
+    },
+    {
+      username: 'elonmusk',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg',
+    },
+    {
+      username: 'Jared',
+      image: 'https://pbs.twimg.com/media/EK2imELWwAA3FQt.jpg:large',
+    },
+
+    {
+      username: 'mrwhostheboss',
+      image:
+        'https://yt3.ggpht.com/Ikb1C4ih2VMvfjma8OO5b39JnHL2CQcQgksB_I7TM-gGA3ERTY589OeLKCYyRQQO0nkE54-f=s900-c-k-c0x00ffffff-no-rj',
+    },
+  ],
+  suggestions: [
+    {
+      username: 'Chip Z',
+      image:
+        'https://pbs.twimg.com/profile_images/1492902736592969732/asuDqTgR_400x400.jpg',
+      text: 'Followed by testuser + 16 more',
+    },
+    {
+      username: 'cleverdude',
+      image:
+        'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
+      text: 'Followed by Julie + 22 more',
+    },
+    {
+      username: 'zucky',
+      image:
+        'https://i.insider.com/6329911dedbe7f00190ef740?width=1136&format=jpeg',
+      text: 'Followed by John P + 2 more',
+    },
+    {
+      username: 'elonmusk',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg',
+      text: 'Followed by Kelly + 10 more',
+    },
+    {
+      username: 'Jared',
+      image: 'https://pbs.twimg.com/media/EK2imELWwAA3FQt.jpg:large',
+      text: 'Followed by elonmusk + 7 more',
+    },
+  ],
+}
